3dc1e1b84b33e64097cfd061a8b93a2d1adbe3b9
Paulo Cunha <paulo_cunha1997@outlook.pt>
2018-07-17 21:45:51 +0000

    Improve organization of the 'JS-Basics' section (using folders)

--- 2-JS-basics/script.js
+++ 2-JS-basics/script.js
@@ -1,19 +1,3 @@
 /***********************/
-/* OBJECTS AND METHODS */
+/* CODING CHALLENGE #4 */
 /***********************/
-
-// Using the same object from the introductory lecture (now with a function/method)
-var paulo = {
-    firstName: 'Paulo',
-    lastName: 'Cunha',
-    birthYear: 1997, 
-    hobbies: ['programming', 'walking', 'exercise'], 
-    occupation: 'student',
-    likesCoding: true,
-    calcAge: function() {
-        this.age = 2018 - this.birthYear;
-    } 
-};
-
-paulo.calcAge();
-console.log(paulo);
