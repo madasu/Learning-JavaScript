e11a03a05402b8e8175ee761dcd052aacf54fec2
Paulo Cunha <paulo_cunha1997@outlook.pt>
2018-07-30 21:56:59 +0000

    Will start to solve the 'Advanced JavaScript' coding challenge

---
+++ 3 - Advanced-JS/script.js
@@ -0,0 +1,37 @@
+/***************************************************************/
+/* ADVANCED JAVASCRIPT: OBJECTS AND FUNCTIONS CODING CHALLENGE */
+/***************************************************************/
+
+/*
+
+--- BASIC VERSION OF THE QUIZ ---
+
+* 1. Build a function constructor called 'Question' to describe a question. A question should include: 
+* a) The question itself;
+* b) The answers from which the player can choose the correct one (choose an adequate data structure here: array, object, etc.);
+* c) The correct answer (a number is recommended for this one).
+
+* 2. Create a couple of questions using the constructor;
+
+* 3. Store them all inside an array; 
+
+* 4. Select one random question an log it to the console, together with the possible answers (each question should have a number). Hint: write a method for the question objects to succeed a the task;
+
+* 5. Use the 'prompt' function to ask the user for the correct answer. The user should input the number of the correct answer such as you displayed it on 'task 4';
+
+* 6. Check if the answer chosen is the correct one and print to the console whether the answer is correct or not. Hint: write another method for this;
+
+* 7. Suppose this could would be a plugin for other programmers to use in their code, so make sure that all your code is private and doesn't interfere with the other programmers code. Hint: a special technique that we learned does exactly that. 
+
+--- EXPERT LEVEL OF THE QUIZ --- 
+
+* 8. After you display the result, display the next random question, so that the game never ends. Hint: write a function for this and call it right after displaying the result; 
+
+* 9. Be careful: after the 'task 8', the game literally never ends. So include the option to quit the game if the user writes 'exit' instead of the answer. In this case, don't call the function from 'task 8';
+
+* 10. Track the user's score to make the game more fun. So each time an answer is correct, add 1 point to the score. Hint: do with the tools you fell more comfortable at this point, but the 'power' of closures can be used to complete the task;
+
+* 11. Display the score in the console. Use yet another method for this task.
+
+*/
+
