9abbdbd79704c096b5a53747c0b893f1639a124c
Paulo Cunha <paulo_cunha1997@outlook.pt>
2018-08-13 16:45:33 +0000

    Finish coding challenge #7 (basic level of the quiz) - Udemy's instructor solution

--- 3 - Advanced-JS/script.js
+++ 3 - Advanced-JS/script.js
@@ -1,6 +1,6 @@
-/****************************************************************************/
-/* ADVANCED JAVASCRIPT: OBJECTS AND FUNCTIONS CODING CHALLENGE (MY SOLUTION)*/
-/****************************************************************************/
+/********************************************************************************************/
+/* ADVANCED JAVASCRIPT: OBJECTS AND FUNCTIONS CODING CHALLENGE (UDEMY'S INSTRUCTOR SOLUTION)*/
+/********************************************************************************************/
 
 /*
 
@@ -23,9 +23,72 @@
 
 * 7. Suppose this could would be a plugin for other programmers to use in their code, so make sure that all your code is private and doesn't interfere with the other programmers code. Hint: a special technique that we learned does exactly that. 
 
---- EXPERT LEVEL OF THE QUIZ --- 
+/* ---------------------------------------------------------------------------------------------------- */
 
-* 8. After you display the result, display the next random question, so that the game never ends. Hint: write a function for this and call it right after displaying the result; 
+(function() {
+
+    // Creating the Question function constructor
+    function Question(question, answers, correct) {
+        this.question = question; 
+        this.answers = answers; 
+        this.correct = correct;
+    }
+
+    // Creating the method that selects a random question 
+    Question.prototype.displayQuestion = function() {
+        console.log(this.question);
+
+        for (var i = 0; i < this.answers.length; i++) {
+            console.log(i + ': ' + this.answers[i]);
+        }
+    }
+
+    // Creating the method that checks if an answer is correct
+    Question.prototype.checkAnswer = function(ans) {
+        if (ans === this.correct) {
+            console.log('Correct answer!');
+        } else {
+            console.log('Wrong answer. Try again :)');
+        }
+    }
+
+    // Creating the questions 
+    var q1 = new Question('Is JavaScript the coolest programming language in the world?', 
+                        ['Yes', 'No'], 
+                        0);
+
+    var q2 = new Question('What\'s the name of this course\'s teacher?',
+                        ['John', 'Michael', 'Jonas'],
+                        2);
+
+    var q3 = new Question('What does best describe coding?', 
+                        ['Boring', 'Hard', 'Fun', 'Tedious'],
+                        2);
+
+    // Saving the questions in an array 
+    var questions = [q1, q2, q3];
+
+    // Creating a random number that will be used to select a random question
+    var n = Math.floor(Math.random() * questions.length);
+
+    // Calling the 'displayQuestion' method 
+    questions[n].displayQuestion();
+
+    // Prompt the user for an answer and saving it on a variable 
+    var answer = parseInt(prompt('Please select the correct answer.'));
+
+    // Calling the 'checkAnswer' method
+    questions[n].checkAnswer(answer);
+
+})();
+
+/* ---------------------------------------------------------------------------------------------------- */
+
+/*
+
+--- EXPERT LEVEL OF THE QUIZ ---
+
+* 8. After you display the result, display the next random question, so that the game never ends. Hint: write a function for this and call it right after displaying the result;
 
 * 9. Be careful: after the 'task 8', the game literally never ends. So include the option to quit the game if the user writes 'exit' instead of the answer. In this case, don't call the function from 'task 8';
 
@@ -34,114 +97,3 @@
 * 11. Display the score in the console. Use yet another method for this task.
 
 */
-
-/* ---------------------------------------------------------------------------------------------------- */
-
-/************************************/
-/* --- BASIC VERSION OF THE QUIZ --- */
-/************************************/
-
-/* ---------------------------------------------------------------------------------------------------- */
-
-/**********************************************/
-/* CREATING THE QUESTION FUNCTION CONSTRUCTOR */
-/**********************************************/
-
-
-
-/* ---------------------------------------------------------------------------------------------------- */
-
-/***************************************/
-/* CREATING THE QUESTIONS FOR THE QUIZ */
-/***************************************/
-
-// Storing the questions #1, #2 and #3 in variables
-
-
-// Storing the possible answers for question #1, #2 and #3 in arrays
-
-
-// Pointing out the correct answer for question #1, #2 and #3
-
-
-// Creating the objects for questions #1, #2 and #3
-
-/* ---------------------------------------------------------------------------------------------------- */
-
-/*********************************************/
-/* STORING ALL THE QUESTIONS INSIDE AN ARRAY */
-/*********************************************/
-
-
-/* ---------------------------------------------------------------------------------------------------- */
-
-/*************************************************/
-/* METHOD THAT DISPLAYS THE SCORE IN THE CONSOLE */
-/*************************************************/
-
-
-/* ---------------------------------------------------------------------------------------------------- */
-
-/*******************************************************************************************************/
-/* METHOD THAT SELECTS A RANDOM QUESTION AND LOG IT TO THE CONSOLE, TOGETHER WITH THE POSSIBLE ANSWERS */
-/*******************************************************************************************************/
-
-// Declaring the 'randomQuestion', 'answerCorrect' and 'score' variables on the global scope
-
-// Creating the method that selects a random question 
-        
-/* Declaring a variable containing the returned random question (it will be used on the 'checkAnswer' 
-method) */
-
-/* ---------------------------------------------------------------------------------------------------- */
-
-/********************************************************************/
-/* USING THE PROMPT FUNCTION TO ASK THE USER FOR THE CORRECT ANSWER */
-/********************************************************************/
-
-// The variable 'answer' that will hold the answer typed at the prompt 
-
-/* ---------------------------------------------------------------------------------------------------- */
-
-/**********************************/
-/* CHECK IF THE ANSWER IS CORRECT */
-/**********************************/
-
-// Creating the method that checks if the answer is correct 
-
-// Displaying the current score in the console 
-
-// Calling the 'next question' function
-
-/* ---------------------------------------------------------------------------------------------------- */
-
-/********************************************/
-/* --- EXPERT LEVEL VERSION OF THE QUIZ --- */
-/********************************************/
-
-/* ---------------------------------------------------------------------------------------------------- */
-
-/****************************************/
-/* 'NEXT RANDOM QUESTION' FUNCTIONALITY */
-/****************************************/
-
-// Declaring the fundamental variables associated with the 'next question' functionality
-
-/* Creating the function that selects a 'next random question' after the result of the previous one has 
-been displayed */
-    
-    // Select the next random question 
-
-    // Prompt the user for a new answer 
-
-    // Correcting the new question 
-
-        // Displaying the current score in the console 
-
-        // Calling the 'next question' function
-        
-        // Displaying the current score in the console 
-
-        // Calling the 'next question' function
-
-/* ---------------------------------------------------------------------------------------------------- */
