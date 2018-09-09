5c2382f397023b6db2a1275eed4ba92e25ae0360
Paulo Cunha <paulo_cunha1997@outlook.pt>
2018-09-09 13:58:28 +0000

    Examples of using let & const (syntax and scoping)

---
+++ 4 - ES6/let-const-scoping-examples.js
@@ -0,0 +1,39 @@
+/* ************************************ */
+/* USING LET & CONST - SCOPING EXAMPLES */
+/* ************************************ */
+
+// ES5 version
+function driversLicence5(passedTest) {
+    if (passedTest) {
+        console.log(firstName); // The value of 'firstName' is set to undefined
+        var firstName = 'Paulo';
+        var yearOfBirth = 1997;
+    }
+    console.log(firstName + ', born in ' + yearOfBirth + ', is now officially allowed to drive a car.');
+}
+
+driversLicence5(true);
+
+// ES6 version 
+function driversLicence6(passedTest) {
+    
+    // console.log(firstName); - The value of 'firstName' is said to be not defined
+    let firstName;
+    const yearOfBirth = 1997;
+
+    if (passedTest) {   
+        firstName = 'Paulo';
+    }
+    console.log(firstName + ', born in ' + yearOfBirth + ', is now officially allowed to drive a car.');
+}
+
+driversLicence6(true);
+
+// More practical example of ES6's syntax
+let i = 23;
+
+for (let i = 0; i < 5; i++) {
+    console.log(i);
+}
+
+console.log(i);
---
+++ 4 - ES6/let-const-syntax-example.js
@@ -0,0 +1,15 @@
+/* ********************************** */
+/* USING LET & CONST - SYNTAX EXAMPLE */
+/* ********************************** */
+
+// ES5 version 
+var name5 = 'Paulo Cunha';
+var age5 = 21;
+name5 = 'Henrique Costa';
+console.log(name5);
+
+// ES6 version 
+const name6 = 'Paulo Cunha';
+let age6 = 21;
+name6 = 'Henrique Costa';
+console.log(name6);
