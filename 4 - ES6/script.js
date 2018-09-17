fa0a781f39c06720698e8eb82dcb45f4aee556f9
Paulo Cunha <paulo_cunha1997@outlook.pt>
2018-09-17 14:51:15 +0000

    Instructions for the ES6 coding challenge (challenge #8 globally)

---
+++ 4 - ES6/script.js
@@ -0,0 +1,24 @@
+/**************************/
+/* ES6 - CODING CHALLENGE */
+/**************************/
+
+/*
+
+* Suppose that you're working in a small town admnistration and you're in charge of two town elements: 
+* 1. Parks
+* 2. Streets
+
+* It's a very small town, so right now there are only 3 parks and 4 streets. All parks and streets have a name an a build year. 
+
+* At the end-of-year meeting, your boss wants a final report with the following:
+* 1. Tree density of each park in the town (formula: number of trees/park area)
+* 2. Average age of each town's park (formula: sum of all ages/number of parks)
+* 3. The name of the park that has more than 1000 trees
+* 4. Total and average length of the town's streets 
+* 5. Size classification of all streets: tiny/small/normal/big/huge (if the size is unknown, the default is normal)
+
+* All the report data should be printed to the console. 
+
+HINT: Use some of the ES6 features: classes, subclasses, template strings, default parameters, maps, arrow functions, destructuring, etc.
+
+*/
