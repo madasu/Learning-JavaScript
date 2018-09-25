97015d3285f82011a1b716226672716213281f46
Paulo Cunha <paulo_cunha1997@outlook.pt>
2018-09-25 09:25:55 +0000

    Example of using asynchronous JS (the 'old' way - using callbacks)

---
+++ 5 - asynchronous-JS/async-js-timer-example.js
@@ -0,0 +1,18 @@
+/****************************************/
+/* EXAMPLE OF 'ASYNCHRONOUS' JAVASCRIPT */
+/****************************************/
+
+const second = () => {
+
+        // Setting a timer that will be called after both 'Hey there' and 'The end' have been printed
+        setTimeout(() => {
+            console.log('Async Hey there');
+        }, 2000);
+    }
+    
+    const first = () => {
+        console.log('Hey there');
+        second();
+        console.log('The end');
+    }
+    first();
